d29c7d8f534d3a585d326cc3d331fbdae9c0a0ea
Felipe Augusto <57400329+FelipeAugus@users.noreply.github.com>
2022-02-15 22:47:15 -0300

    Script no front encaminhado

--- src/Views/Scripts/scripts.js
+++ src/Views/Scripts/scripts.js
@@ -1,19 +1,36 @@
-let retRequest;
-let timeRequest = null; 
 
-async function carregaFilmes() {
-    makeRequest('filmes');
-    
-    let req;
-    setTimeout(()=>{
-        req = retRequest;
-        console.log(req);
-        // PREENCHE A TABELINHA SHWERNOUS
-    }, 5000);
+function carregaFilmes() {
+    // Não recomendo fazer desta forma. É gambiarra. 
+    makeRequest({url: 'filmes', params: {'rota': 'UPDATE', id: 1, nome: "FilmeTeste01", imagem_filme: "n"}})
+
+    const filmes = makeRequest({url: 'filmes', params: {'rota': 'SELECT'}})
+
+    const table = document.getElementById('tabela');
+    let clock = true;
+    filmes.forEach(filme => {
+        console.log(filme);
+        const linha = table.insertRow();
+
+        if(clock){
+            clock = !clock;
+            linha.className = 'dif';
+        }
+
+        const colunaImg = linha.insertCell();
+        colunaImg.appendChild(document.createTextNode(filme.imagem_filme))
+
+        const colunaNome = linha.insertCell();
+        colunaNome.appendChild(document.createTextNode(filme.nome))
+
+        const colunaDuracao = linha.insertCell();
+        colunaDuracao.appendChild(document.createTextNode(filme.tempo_minutos_filme))
+    });
 }
 
-async function makeRequest(url) {
-    retRequest = false;
+function makeRequest(req) {
+    const url = req.url;
+    const params = req.params || null;
+
     if (window.XMLHttpRequest) {
         httpRequest = new XMLHttpRequest();
     } else if (window.ActiveXObject) { // IE
@@ -26,48 +43,19 @@ async function makeRequest(url) {
         return false;
     }
 
-    httpRequest.onreadystatechange = alertContents;
-    httpRequest.open('GET', url);
-    httpRequest.send();
-
-    if (timeRequest === null){
-        timeRequest = setInterval(() => {
-            console.log(!!httpRequest.responseText == true, typeof(httpRequest.responseText));
-            if(retRequest){
-                clearInterval(timeRequest)
-                console.log(retRequest);
-                return retRequest;
-            }
-            console.log('Aguardando requisição'); 
-        }, 2000)
-        setTimeout(() => {
-            clearInterval(timeRequest);
-        }, 30000)
-    }
-}
-
-function alertContents() {
+    // httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('POST', url, false);
+    
+    httpRequest.setRequestHeader('Content-Type', 'application/json');
     try {
+        console.log(params, typeof(params), JSON.stringify(params), typeof(JSON.stringify(params)))
+        httpRequest.send(JSON.stringify(params));
         if (httpRequest.readyState === 4) {
             if (httpRequest.status === 200) {
-                // alert(httpRequest.responseText);
-                retRequest = httpRequest.responseText
-            } else {
-                clearInterval(timeRequest)
-                alert('Ocorreu um problema com a requisição, STATUS REQUISIÇÃO '+String.toString(httpRequest.status));
-                retRequest = 'Erro na requisição '+String.toString(httpRequest.status);
+                return JSON.parse(httpRequest.responseText);
             }
         }
-    }
-    catch (e) {
-        alert('Caught Exception: ' + e.description);
-    }
-}
-
-function syncDelay(milliseconds){
-    let start = new Date().getTime();
-    let end = 0;
-    while( (end-start) < milliseconds) {
-        end = new Date().getTime();
+    } catch (error) {
+        alert('Ocorreu um problema com a requisição, STATUS REQUISIÇÃO '+String.toString(httpRequest.status)); 
     }
 }
